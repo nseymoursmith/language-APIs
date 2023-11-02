c622581b99211e021046532a4a1deb02d5238421
nseymoursmith <nseymoursmith@gmail.com>
2023-11-02 16:54:34 +0000

    [BUGFIX] Run docker from correct directory in deployment

--- deploy-flask.sh
+++ deploy-flask.sh
@@ -6,6 +6,12 @@ if [ $? != 0 ]; then
     exit 1
 fi
 
+if ! [ -d "$HOME/language-APIs/nic/flask/" ]; then
+    echo 'repo does not exist!'
+    exit 1
+fi
+
+cd $HOME/language-APIs/nic/flask/
 docker compose up -- build -d
 if [ $? != 0 ]; then
     echo "docker compose failed, check the server logs!"
